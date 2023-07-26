e89ff27238d980d330588cb2c5cea75408578a0c
unknown <devkdhee@gmail.com>
2023-07-26 16:56:47 +0900

    feat : 싱글 컴포넌트에서 새 컴포넌트 등록하기

---
+++ vue-cli/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+<!-- vue2 버전은 루트 엘리먼트 div가 하나만 있어야한다 -->
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <!-- 파스칼 케이스 -->
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- 케밥 기법 -->
+    <!-- <hello-world></hello-world>  -->
+    <div>
+      <!-- {{ str }} -->
+      <app-header></app-header>
+    </div>
+    
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import AppHeader from './components/AppHeader.vue'
+
+export default { // new vue({})
+  name: 'App',
+  components: {
+    HelloWorld, // = 'hello-word' : HelloWorld 
+    AppHeader
+  },
+  data : function(){
+    return {
+      str : 'Header'
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>

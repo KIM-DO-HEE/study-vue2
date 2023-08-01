1278a6ae078ea85d0dc50be934d791b7fd3795de
unknown <devkdhee@gmail.com>
2023-08-01 11:54:37 +0900

    feat : 할일 목록 표시 localStorage에 저장된 할 일 목록을 v-for 속성을 사용하여 목록 표시

--- vue-todo/src/components/TodoList.vue
+++ vue-todo/src/components/TodoList.vue
@@ -1,11 +1,30 @@
 <template>
   <div>
-    List
+    <ul>
+      <li v-for="todoItem in todoItems" v-bind:key="todoItem">{{ todoItem }}</li>
+    </ul>
+
+    <!-- {{ todoItems }} -->
   </div>
 </template>
 
 <script>
-export default {
+export default { 
+  created: function(){
+    if(localStorage.length > 0 ){
+      for ( var i = 0; i < localStorage.length; i ++){
+        this.todoItems.push(localStorage.key(i))
+        console.log(localStorage.key(i))
+
+      }
+    }
+  },
+
+  data : function() {
+    return {
+      todoItems : []
+    }
+  },
 
 }
 </script>

08555029d20134081940980c87a11d2baad898bc
unknown <devkdhee@gmail.com>
2023-08-01 15:02:40 +0900

    feat : 할 일 삭제 기능 구현 localStorage에 저장된 할 일 목록 중 선택한 할 일을 삭제하는 기능 추가

--- vue-todo/src/components/TodoList.vue
+++ vue-todo/src/components/TodoList.vue
@@ -1,7 +1,12 @@
 <template>
   <div>
     <ul>
-      <li v-for="todoItem in todoItems" v-bind:key="todoItem">{{ todoItem }}</li>
+      <li v-for="(todoItem, index) in todoItems" v-bind:key="todoItem" class="shadow">
+        {{ todoItem }}
+        <span class="removeBtn" v-on:click="removeTodo(todoItem, index)">
+          <i class="fas fa-trash-alt"></i>
+        </span>
+      </li>
     </ul>
 
     <!-- {{ todoItems }} -->
@@ -25,10 +30,55 @@ export default {
       todoItems : []
     }
   },
+  methods : {
+    removeTodo : function(todoItem, index){
+      console.log(todoItem, index)
+      localStorage.removeItem(todoItem);
+      this.todoItems.splice(index, 1);
+    }
+  }
 
 }
 </script>
 
 <style>
+ul{
+  list-style-type: none;
+  padding-left: 0px;
+  margin-top: 0;
+  text-align: left;
+}
 
+li{
+  display : flex;
+  min-height: 50px;
+  height :50px;
+  line-height: 50px;
+  margin : 0.5rem 0;
+  padding : 0 0.9rem;
+  background: white;
+  border-radius: 5px;
+}
+
+.checkBtn{
+  line-height: 45px;
+  color : #65acde ;
+  margin-right : 5px;
+}
+
+.checkBtnCompleted{
+  color : #b3adad;
+}
+
+.textCompleted{
+  text-decoration: line-through;
+  color: #b3adad;
+}
+
+.removeBtn{
+  margin-left : auto;
+  color : #de4343;
+}
 </style>
+
+

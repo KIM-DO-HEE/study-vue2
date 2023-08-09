a39902b0ddd2f20c056162de2880071987f8693e
unknown <devkdhee@gmail.com>
2023-08-09 16:05:33 +0900

    refactor : axios를 사용하여 데이터 요청 axios를 사용하여 데이터를 전달받아 화면에 출력

--- vue-news/src/views/AskView.vue
+++ vue-news/src/views/AskView.vue
@@ -1,9 +1,29 @@
 <template>
-  <div>ask</div>
+  <div>
+    <div v-for="ask in asks">{{ ask.title }}</div>
+ </div>
 </template>
 
 <script>
+import { fetchAskList } from '../api/index.js'
 export default {
+    data(){
+        return {
+            asks : []
+        }
+    },
+    created(){
+        console.log('호출 전' + this)
+        var vm = this;
+        fetchAskList()
+        .then(response => {
+            console.log('호출 후' + this)
+            vm.asks = response.data;
+        })
+        .catch(error => {
+            console.log(error)
+        });
+    }
 
 }
 </script>
